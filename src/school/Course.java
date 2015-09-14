ff2d8e1cfaa67a27599e971977999ae5fd3d7382
unknown <jeffreyyee@mvhsrop901-teach.vusdnoc.vusd.k12.ca.us>
2015-09-14 11:09:17 -0700

    add the Course class

---
+++ src/school/Course.java
@@ -0,0 +1,69 @@
+
+package school;
+import java.util.ArrayList;
+
+public class Course {
+    enum Type {
+        Math,Science,English,History,Language,Elective,PE
+    }
+    private static ArrayList<Course> courses = new ArrayList<Course>();
+    private String name;
+    private Type type;
+    private int period;
+
+    public static Course addCourse(String _name,
+    Type _type, int _period)
+    {
+        Course temp = new Course(_name,_type,_period);
+        courses.add(temp);
+        return(temp);
+    }
+    Course()
+    {
+        name = "None";
+        type = Type.Elective;
+        period = 1;
+    }
+    Course(String _name,Type _type, int _period)
+    {
+        name = _name;
+        type = _type;
+        period = _period;
+    }   
+    public void setPeriod(int _period)
+    {
+        period = _period;
+    }
+    public int getPeriod()
+    {
+        return(period);
+    }       
+    public void setName(String _name)
+    {
+        name = _name;
+    }
+    public String getName()
+    {
+        return(name);
+    }    
+    public void setType(Type _type)
+    {
+        type = _type;
+    }
+    public Type getType()
+    {
+        return(type);
+    }  
+    public static void printNames()
+    {
+        System.out.println("===printNames===");
+        for (Course temp : courses)
+        {
+                System.out.println(temp.getName());
+        }        
+    }    
+    public String toString()
+    {
+        return(name + " " + type + " " + period);
+    }    
+}
--- src/school/School.java
+++ src/school/School.java
@@ -1,18 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package school;
-
-/**
- *
- * @author jeffreyyee
- */
 public class School {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Person joe = 
         Person.addPerson("Joe",Person.Gender.Male,120);
@@ -29,5 +16,13 @@ public class School {
         Person.printNames(Person.Gender.Female);
         
         System.out.println(billyJoe);
+        
+        Course geom = 
+        Course.addCourse("Geometry", Course.Type.Math, 1);
+        Course photo = 
+        Course.addCourse("Photography", Course.Type.Elective, 3);
+        Course.printNames();
+        System.out.println(photo);
+        
     }
 }
